f7cda71af4e36d09ab77dbdefe9639036f36bf05
SniverDaBest <owen.gary629@gmail.com>
2025-04-08 20:48:32 -0700

    Removed a bunch of stuff that the kernel should handle, not the bootloader.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,18 @@
 #![no_std]
 #![no_main]
-#![feature(alloc_error_handler, abi_x86_interrupt, core_intrinsics)]
 #![allow(
-    static_mut_refs,
     clippy::needless_return,
-    unsafe_op_in_unsafe_fn,
-    clippy::missing_safety_doc
 )]
 
-extern crate alloc;
-
-pub mod display;
-pub mod fonts;
-
-use alloc::vec::*;
-use display::{Buffer, TTY, TTYColors};
-use fontdue::{Font, FontSettings};
-use log::{error, info};
-use spinning_top::Spinlock;
+pub mod serial;
 use uefi::{
-    CString16,
-    fs::{FileSystem, FileSystemResult},
-    helpers,
-    prelude::*,
-    proto::console::gop::{BltPixel, GraphicsOutput, Mode, ModeIter},
+    boot::{exit_boot_services, MemoryType}, helpers, prelude::*, proto::console::gop::{GraphicsOutput, Mode, ModeIter},
 };
-
-pub const LEMONCAKE_VER: &str = "25m3-UEFI";
-pub static TERM: Spinlock<TTY> = Spinlock::new(TTY::new());
 
 fn get_good_mode(modes: ModeIter) -> Mode {
     for m in modes {
-        if m.info().resolution() == (1920, 1080) {
-            info!("Found good mode:\n{:#?}", m);
+        if m.info().resolution() == (640, 480) {
+            serial_println!("Found good mode:\n{:#?}", m);
             return m;
         }
     }
@@ -40,15 +20,8 @@ fn get_good_mode(modes: ModeIter) -> Mode {
     panic!("Couldn't find a good mode!");
 }
 
-pub fn read_file(path: &str) -> FileSystemResult<Vec<u8>> {
-    let path = CString16::try_from(path).expect("Unable to convert path (&str) to CString16!");
-    let fs = boot::get_image_file_system(boot::image_handle())
-        .expect("Unable to get image file system!");
-    let mut fs = FileSystem::new(fs);
-    fs.read(path.as_ref())
-}
-
 #[entry]
+#[allow(mutable_transmutes)]
 fn main() -> Status {
     helpers::init().unwrap();
 
@@ -57,28 +30,23 @@ fn main() -> Status {
     let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle)
         .expect("Unable to get the GOP!");
     let mode = get_good_mode(gop.modes());
-
     gop.set_mode(&mode).expect("Unable to set GOP mode!");
 
-    let mut buf = Buffer::new(&mut gop, 1920, 1080);
-    buf.fill_buffer(BltPixel::new(0, 0, 0))
-        .expect("Unable to clean the screen!");
+    let mut fb = gop.frame_buffer();
+    let fb_addr = fb.as_mut_ptr();
+    let res = gop.current_mode_info().resolution();
 
-    let font_data = read_file("font.ttf").expect("Unable to read font file!");
+    serial_println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\nFramebuffer info:\nAddress: {:?}\nResolution: {}x{}\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%", fb_addr, res.0, res.1);
+    serial_println!("Booting Lemoncake...");
 
-    let f = Font::from_bytes(font_data.as_slice(), FontSettings::default())
-        .expect("Unable to create font from bytes!");
+    let _mmap = unsafe { exit_boot_services(MemoryType::LOADER_DATA) };
 
-    TERM.lock().set_colors(TTYColors::default());
-    TERM.lock().write_str(&mut buf, &f, "god this text is mangled lol\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{}]\\|;:'\",<.>/?").expect("Unable to write text to TTY!");
-
-    info!("Done!");
-    loop {}
+    panic!("uhh i haven't implemented this yet.");
 }
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    error!(
+    serial_println!(
         "(X_X)\n\nUh-Oh, Lemoncake panicked!\nMessage: {}\nLocation: {}@L{}:{}",
         info.message(),
         info.location().unwrap().file(),
